59994601f6194a3a49e7a83d1332e63865e96a9c
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-11-03 17:22:05 +0200

    add mine.js

---
+++ static/js/mine.js
@@ -0,0 +1,8 @@
+function mine_main(){
+    mainframe = document.getElementById("mainframe")
+    mainframe.innerHTML = ""
+    document.title = "My questionnares";
+    h1 = document.createElement('h1')
+    h1.innerHTML = 'My questionnares';
+    mainframe.appendChild(h1)
+}
