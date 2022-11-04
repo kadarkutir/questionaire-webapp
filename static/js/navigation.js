df876841d78cd3f88cc549e81b95db24b7766ac5
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-11-04 17:18:58 +0200

    add navigation.js

---
+++ static/js/navigation.js
@@ -0,0 +1,25 @@
+function openNav(){
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+document.addEventListener('mouseup', function(e) {
+    var container = document.getElementById('mySidenav');
+    if (!container.contains(e.target)) {
+        container.style.width = "0";
+    }
+});
+
+function openNavProfile(){
+    document.getElementById("mySidenavProfile").style.width = "250px";
+}
+
+document.addEventListener('mouseup', function(e) {
+    var container2 = document.getElementById('mySidenavProfile');
+    if (!container2.contains(e.target)) {
+        container2.style.width = "0";
+    }
+});
+
+function closeNavProfile(){
+    document.getElementById("mySidenavProfile").style.width = "0";
+}
