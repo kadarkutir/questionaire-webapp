16587bc9a72ab488dfc53a941b98c117f849ab92
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-11-04 16:18:03 +0100

    Delete mine.js

--- static/js/mine.js
+++
@@ -1,8 +0,0 @@
-function mine_main(){
-    mainframe = document.getElementById("mainframe")
-    mainframe.innerHTML = ""
-    document.title = "My questionnares";
-    h1 = document.createElement('h1')
-    h1.innerHTML = 'My questionnares';
-    mainframe.appendChild(h1)
-}
