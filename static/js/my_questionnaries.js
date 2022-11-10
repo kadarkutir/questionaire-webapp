eb09658532f1a9c69e829a3d1cec6510230cbd4a
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-11-10 15:55:57 +0100

    Update my_questionnaries.js

--- static/js/my_questionnaries.js
+++ static/js/my_questionnaries.js
@@ -1,25 +1,4 @@
-function get_my_questionnaries(){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET","/get_own_questionnaries_by_user",true)
-    xmlHttp.setRequestHeader("Content-Type","application/json;charset=UTF-8");
-    xmlHttp.onload = function(){
-        rsp = JSON.parse(xmlHttp.response)
-        create_table_mine(rsp)
-    }
-    xmlHttp.send()
-}
-
-function get_answers_for_questionnare(link){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET",link,true)
-    xmlHttp.setRequestHeader("Content-Type","application/json;charset=UTF-8");
-    xmlHttp.onload = function(){
-        rsp2 = JSON.parse(xmlHttp.response)
-        create_answers_by_user(rsp2)
-    }
-    xmlHttp.send()
-}
-
+// Answer on questionnare table creator
 function get_answers_on_own_questionnare(link){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET",link,true)
@@ -29,61 +8,6 @@ function get_answers_on_own_questionnare(link){
         create_table_for_answers(rsp3)
     }
     xmlHttp.send()
-}
-
-function create_answers_by_user(data2){
-    mainframe = document.getElementById("mainframe")
-    mainframe.innerHTML = ""
-    h1 = document.createElement('h1')
-    h1.innerHTML = data2[0];
-    mainframe.appendChild(h1)
-
-    answer_p = document.createElement("p")
-    answer_p.innerHTML = "Answered by: "+data2[1]
-    answer_p.classList.add("my_q_answer_by")
-    mainframe.appendChild(answer_p)
-    
-
-    ans_table = document.createElement('table')
-    ans_table.classList.add('result_table_design')
-    ans_tbody = document.createElement('tbody')
-
-    for(let j=2;j<data2.length;j++){
-        row_ans = document.createElement('tr')
-        col_ans = document.createElement('td')
-        col_ans.appendChild(document.createTextNode(data2[j]))
-        row_ans.appendChild(col_ans)
-        ans_tbody.appendChild(row_ans)
-    }
-
-    back_button = document.createElement('button')
-    back_button.classList.add('back_button')
-    back_button.addEventListener('click',() => {
-        my_questionnaries_main()
-    })
-    
-
-    ans_table.appendChild(ans_tbody)
-    mainframe.appendChild(ans_table)
-    mainframe.appendChild(back_button)
-}
-
-function makeCell_my_questionnaire(value,title){
-    cell = document.createElement('td')
-    cell.appendChild(document.createTextNode(value))
-    cell.addEventListener('click',function(){
-        get_answers_on_own_questionnare("/get_all_answers_on_questionnare/"+title)
-    })
-
-    return cell
-}
-
-function makeRow_my_questionnaire(tbody,values){
-    r = document.createElement('tr')
-    r.appendChild(makeCell_my_questionnaire(values[0],values[0]))
-    r.appendChild(makeCell_my_questionnaire(values[1],values[0]))
-    
-    tbody.appendChild(r)
 }
 
 function makeCell_answers(value,title,user){
@@ -161,7 +85,84 @@ function create_table_for_answers(data3){
     }       
 }
 
+// Answer by user creator
+function get_answers_for_questionnare(link){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET",link,true)
+    xmlHttp.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    xmlHttp.onload = function(){
+        rsp2 = JSON.parse(xmlHttp.response)
+        create_answers_by_user(rsp2)
+    }
+    xmlHttp.send()
+}
 
+function create_answers_by_user(data2){
+    mainframe = document.getElementById("mainframe")
+    mainframe.innerHTML = ""
+    h1 = document.createElement('h1')
+    h1.innerHTML = data2[0];
+    mainframe.appendChild(h1)
+
+    answer_p = document.createElement("p")
+    answer_p.innerHTML = "Answered by: "+data2[1]
+    answer_p.classList.add("my_q_answer_by")
+    mainframe.appendChild(answer_p)
+    
+
+    ans_table = document.createElement('table')
+    ans_table.classList.add('result_table_design')
+    ans_tbody = document.createElement('tbody')
+
+    for(let j=2;j<data2.length;j++){
+        row_ans = document.createElement('tr')
+        col_ans = document.createElement('td')
+        col_ans.appendChild(document.createTextNode(data2[j]))
+        row_ans.appendChild(col_ans)
+        ans_tbody.appendChild(row_ans)
+    }
+
+    back_button = document.createElement('button')
+    back_button.classList.add('back_button')
+    back_button.addEventListener('click',() => {
+        my_questionnaries_main()
+    })
+    
+
+    ans_table.appendChild(ans_tbody)
+    mainframe.appendChild(ans_table)
+    mainframe.appendChild(back_button)
+}
+
+// My questionnare table creator
+function get_my_questionnaries(){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET","/get_own_questionnaries_by_user",true)
+    xmlHttp.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    xmlHttp.onload = function(){
+        rsp = JSON.parse(xmlHttp.response)
+        create_table_mine(rsp)
+    }
+    xmlHttp.send()
+}
+
+function makeCell_my_questionnaire(value,title){
+    cell = document.createElement('td')
+    cell.appendChild(document.createTextNode(value))
+    cell.addEventListener('click',function(){
+        get_answers_on_own_questionnare("/get_all_answers_on_questionnare/"+title)
+    })
+
+    return cell
+}
+
+function makeRow_my_questionnaire(tbody,values){
+    r = document.createElement('tr')
+    r.appendChild(makeCell_my_questionnaire(values[0],values[0]))
+    r.appendChild(makeCell_my_questionnaire(values[1],values[0]))
+    
+    tbody.appendChild(r)
+}
 
 function create_table_mine(data){
     if(data.length == 0){
@@ -195,6 +196,7 @@ function create_table_mine(data){
     }
 }
 
+// Questionnare creator function
 function create_questions_for_questionnare(){
     mainframe = document.getElementById('mainframe')
     mainframe.innerHTML = ""
@@ -256,7 +258,7 @@ function create_questions_for_questionnare(){
     mainframe.appendChild(back_button)
 }
 
-
+// Main function
 function my_questionnaries_main(){
     mainframe = document.getElementById("mainframe")
     mainframe.innerHTML = ""
